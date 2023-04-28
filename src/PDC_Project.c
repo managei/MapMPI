00ec82dfce80b0edc26b5c37043f8e05e8366282
Lazer 430 <fasihabdullah430@gmail.com>
2023-04-29 03:29:59 +0500

    added run commands

--- src/PDC_Project.c
+++ src/PDC_Project.c
@@ -1,3 +1,5 @@
+// to compile: mpicc PDC_Project.c
+// to run: mpiexec -n 8 ./a.out
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
@@ -7,7 +9,7 @@ void main(int argc, char *argv[])
     MPI_Init(&argc, &argv);
     int rank = -1;
     int numberOfProcesses = 0;
-
+    char processorName[MPI_MAX_PROCESSOR_NAME];
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcesses);
 
