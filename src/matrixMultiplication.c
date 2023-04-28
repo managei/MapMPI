f050355faa7959d5ac00e010ef6364e62ac78dce
farazrazi <farazrazi432@gmail.com>
2023-04-29 01:38:28 +0500

    seperated file creation and matrix multiplication

--- src/matrixMultiplication.c
+++ src/matrixMultiplication.c
@@ -2,51 +2,21 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 
-void writeMatrixToFile(int matrixSize, char *file)
-{
-    int i,j,num=0;
-    printf("Writing to %s\n",file);
-    FILE * fp = fopen(file, "w+"); // open the file for reading
+// TO COMPILE: 
+// gcc matrixMultiplication.c -lm -o matrixMultiplication
 
-    for(i=0;i<matrixSize;i++)
-    {
-        for(j=0;j<matrixSize;j++)
-        { 
-            fprintf(fp,"%d ",num);
-            num++;
-        }
-        fprintf(fp,"\n");
-    }
-    printf("Data written to %s\n",file);
-    fclose(fp);
-}
-
-void writeMatrixToFileBinary(int matrixSize, char *file)
-{
-    int i,j,num=rand() % 5;
-    printf("Writing to %s\n",file);
-    FILE * fp = fopen(file, "wb"); // open the file for reading
-
-    for(i=0;i<matrixSize;i++)
-    {
-        for(j=0;j<matrixSize;j++)
-        { 
-            fwrite(&num,sizeof(int),1,fp);
-            num++;
-        }
-    }
-    printf("Data written to %s\n",file);
-    fclose(fp);
-}
+// TO RUN: only the size of matrix : should already be created
+// ./matrixMultiplication 3
 
 void read_matrix(char* filename, int rows, int cols, int **matrix, int matrixSize) {
     // Open the file for reading
    FILE* fp = fopen(filename, "r");
 
     // Read the number of rows and columns
-//    fscanf(fp, "%d %d", &rows, &cols);
-//    printf("Rows: %d\nCols: %d\n",rows,cols);
+    // fscanf(fp, "%d %d", &rows, &cols);
+    // printf("Rows: %d\nCols: %d\n",rows,cols);
 
     // Read the matrix elements
     for (int i = 0; i < matrixSize; i++) {
@@ -95,27 +65,32 @@ void serialMultiply(int **matA, int**matB, int **matC, int N)
     }
 }
 
-
 int main(int argc, char* argv[]) {
-    srand(time(NULL));
-    //int power = atoi(argv[1]); //maximum is 2^30
-    //printf("Power: %d\n",power);
-    //int matrixSize = 0;//rand() % (1 << 16 - 1 - 12 + 1) + (1 << 12); 
 
+    srand(time(NULL));
+    if (argc != 2)
+    {
+        printf("Please Provid Valid Arguments: \n 1. Power of 2 for matrix size \n");
+        return -1;
+    }
+
+    int power = atoi(argv[1]); //maximum is 2^30
     //randomly calculating matrix size
-    int power = 3;//rand() % 12 + (30-12) ;
+    // int power = 3;//rand() % 12 + (30-12) ;
     printf("Power: %d\n",power);
     int matrixSize = pow(2,power);
     printf("Matrix Size: %d\n",matrixSize);
 
-    //now we will create a file
-    char *file1 = argv[1];
-    char *file2 = argv[2];
-    printf("File 1 name: %s\n",file1);
-    printf("File 2 name: %s\n",file2);
 
-    writeMatrixToFile(matrixSize,file1);
-    writeMatrixToFile(matrixSize,file2);
+    // append datasets directory to file name in front 
+    char file1[] = "../datasets/matrixA-";
+    char file2[] = "../datasets/matrixB-";
+
+    // strncat(filename, &power, powerSize);
+    char powerStr[10];
+    sprintf(powerStr, "%d", power);
+    strncat(file1, powerStr, strlen(powerStr));
+    strncat(file2, powerStr, strlen(powerStr));
 
     int** matA = (int**) calloc(matrixSize, sizeof(int*));
     int** matB = (int**) calloc(matrixSize, sizeof(int*));
