e49ddaf6d0981d7102f22ab4a7714756c4d21366
Lazer 430 <fasihabdullah430@gmail.com>
2023-04-29 12:21:48 +0500

    added comm for mapper and reducer

--- src/PDC_Project.c
+++ src/PDC_Project.c
@@ -7,7 +7,7 @@
 #include <math.h>
 #include <time.h>
 
-void calculateNumberOfMappersAndReducers(long int sizeOfMatrix, int numberOfSlaves, int *mappers, int *reducers, long int *inputSplit, bool *equalDivisionPossible)
+void calculateNumberOfMappersAndReducers(long int sizeOfMatrix, int numberOfSlaves, int *mappers, int *reducers, long long int *inputSplit, int *equalDivisionPossible, int *remainingSplit)
 {
     long long int totalElementsInMatrix = sizeOfMatrix * sizeOfMatrix;
     int numberOfMappers = numberOfSlaves - 1;          // at least one slave is required for reducer
@@ -17,8 +17,8 @@ void calculateNumberOfMappersAndReducers(long int sizeOfMatrix, int numberOfSlav
     {
         numberOfMappers--;
         mod = totalElementsInMatrix % numberOfMappers;
-        printf("mod: %d\n", mod);
-        printf("numberOfMappers: %d\n", numberOfMappers);
+        // printf("mod: %d\n", mod);
+        // printf("numberOfMappers: %d\n", numberOfMappers);
     }
 
     *mappers = numberOfMappers;
@@ -27,9 +27,15 @@ void calculateNumberOfMappersAndReducers(long int sizeOfMatrix, int numberOfSlav
     mod = totalElementsInMatrix % numberOfMappers;
 
     if (mod == 0)
-        *equalDivisionPossible = true;
+    {
+        *equalDivisionPossible = 1;
+        *remainingSplit = 0;
+    }
     else
-        *equalDivisionPossible = false;
+    {
+        *equalDivisionPossible = 0;
+        *remainingSplit = mod;
+    }
 
     return;
 }
@@ -41,31 +47,77 @@ void main(int argc, char *argv[])
     int rank = -1;
     int numberOfProcesses = 0;
     char processorName[MPI_MAX_PROCESSOR_NAME];
-    int lengthOfProcessorName = MPI_MAX_PROCESSOR_NAME;
-
-    long int sizeOfMatrix = pow(2, rand() % 20 + 12); // assumed size of matrix
+    int lengthOfProcessorName = 0;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcesses);
     MPI_Get_processor_name(processorName, &lengthOfProcessorName);
 
+    long long int sizeOfMatrix = 0;
+    int mappers = 0;
+    int reducers = 0;
+    int equalDivisionPossible = 0;
+
     if (rank == 0)
     {
-        int mappers = 0;
-        int reducers = 0;
-        long int inputSplit = 0;
-        bool equalDivisionPossible = false;
-        calculateNumberOfMappersAndReducers(sizeOfMatrix, numberOfProcesses - 1, &mappers, &reducers, &inputSplit, &equalDivisionPossible);
         printf("Master with running id %d running on %s\n", rank, processorName);
-        printf("Size of matrix: %ld\n", sizeOfMatrix);
+
+        sizeOfMatrix = pow(2, rand() % 20 + 12); // assumed size of matrix
+        long long int inputSplit = 0;
+        int remainingSplit = 0;
+        calculateNumberOfMappersAndReducers(sizeOfMatrix, numberOfProcesses - 1, &mappers, &reducers, &inputSplit, &equalDivisionPossible, &remainingSplit);
+
+        printf("Size of matrix: %lld\n", sizeOfMatrix);
         printf("Number of mappers: %d\n", mappers);
         printf("Number of reducers: %d\n", reducers);
-        printf("Input split: %ld\n", inputSplit);
+        printf("Input split: %lld\n", inputSplit);
+        printf("Remaining split: %d\n", remainingSplit);
         printf("Equal division possible: %s\n", equalDivisionPossible ? "true" : "false");
     }
-    else
+    MPI_Bcast(&sizeOfMatrix, 1, MPI_LONG_LONG_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&mappers, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    reducers = (numberOfProcesses - 1) - mappers;
+
+    MPI_Group MPI_GROUP_WORLD;
+    MPI_Comm_group(MPI_COMM_WORLD, &MPI_GROUP_WORLD); // fetch group from MPI_COMM_WORLD
+
+    if (rank > 0 && rank <= mappers)
     {
-        // printf("This is slave process %d\n", rank);
+        MPI_Comm MPI_COMM_MAPPER;
+        MPI_Group MPI_GROUP_MAPPER;
+        int mapperRankInMapperComm = 0;
+        int mapperRanks[mappers];
+
+        int curMapperRank = 1;
+        for (int i = 0; i < mappers; i++)
+        {
+            mapperRanks[i] = curMapperRank++;
+        }
+        MPI_Group_incl(MPI_GROUP_WORLD, mappers, mapperRanks, &MPI_GROUP_MAPPER); // create group of mappers
+        MPI_Comm_create_group(MPI_COMM_WORLD, MPI_GROUP_MAPPER, 1, &MPI_COMM_MAPPER);
+        MPI_Comm_rank(MPI_COMM_MAPPER, &mapperRankInMapperComm);
+
+        printf("Mapper with running id %d running on %s and mapper rank %d\n", rank, processorName, mapperRankInMapperComm);
+    }
+    else if (rank > mappers)
+    {
+        MPI_Comm MPI_COMM_REDUCER;
+        MPI_Group MPI_GROUP_REDUCER;
+        int reducerRankInReducerComm = 0;
+        int reducerRanks[reducers];
+
+        int curReducerRank = mappers + 1;
+        for (int i = 0; i < reducers; i++)
+        {
+            reducerRanks[i] = curReducerRank++;
+        }
+
+        MPI_Group_incl(MPI_GROUP_WORLD, reducers, reducerRanks, &MPI_GROUP_REDUCER); // create group of reducers
+        MPI_Comm_create_group(MPI_COMM_WORLD, MPI_GROUP_REDUCER, 2, &MPI_COMM_REDUCER);
+        MPI_Comm_rank(MPI_COMM_REDUCER, &reducerRankInReducerComm);
+
+        printf("Reducer with running id %d running on %s and reducer rank %d\n", rank, processorName, reducerRankInReducerComm);
     }
 
     MPI_Finalize();
