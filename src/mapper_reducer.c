499832527d9e2d5e3e3917625a31577e2280ce5d
Zeerak Zubair <zeerakzubair047@gmail.com>
2023-04-29 12:07:44 +0500

    Mapper and Reducer file

---
+++ src/mapper_reducer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//input to mapper
+//MatrixType,Row,Column,Value
+
+void mapper(char matrixType, int row, int col, int value)
+{
+
+}
+
+void reducer()
+{
+
+}
+
+int main()
+{
+
+    return 0;
+}
